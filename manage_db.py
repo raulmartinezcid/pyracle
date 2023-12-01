10c6a7eb6b1bf7241cfa421d254ac7e04d6a4196
rmar <106436240+rmar-dot@users.noreply.github.com>
2023-12-01 19:13:09 +0100

    Update manage_db.py

--- manage_db.py
+++ manage_db.py
@@ -10,29 +10,30 @@ import numpy as np
 import oracledb
 import sqlalchemy as sqla
 import sys
-# si no ponemos esto salta error: ModuleNotFoundError: No module named 'cx_Oracle'
-# pero cs_oracle ya no se usa, hay que usar oracledb
+# we need to put following line so the error: ModuleNotFoundError: No module named 'cx_Oracle'
+# does not appear, but we use oracledb
 oracledb.version = "8.3.0"
 sys.modules["cx_Oracle"] = oracledb 
 
 def get_credentials():
-    # coge credenciales para hacer conexion con oracle
-    # formato: user;passw;db
+    # get credentials from file to make oracle connection
+    # format: user;passw;db
     ruta_login_txt = r'ruta\f.txt' 
 
-    file = open(ruta_login_txt, 'r') # abrimos el fichero txt
+    file = open(ruta_login_txt, 'r') # open txt file
 
-    content = file.read() # leemos el contenido
+    content = file.read() # read content
 
-    lst_login = content.split(';') # separamos los elementos
+    lst_login = content.split(';') # split elements
 
     user, passw, db = lst_login[0], lst_login[1], lst_login[2]
     
     return user, passw , db
 
 
-def read_query(path_query): # lee la query de un codigo .sql en la ruta introducida
-    # abrimos la query, la leemos y cerramos el fichero
+def read_query(path_query): # read full code in a .sql file in path_query
+    # open c.sql, read it and close it
+    # return string with the full code
     sql_file = open(path_query, 'r')
     sql_query = sql_file.read()
     sql_file.close()   
@@ -40,19 +41,20 @@ def read_query(path_query): # lee la query de un codigo .sql en la ruta introduc
 
         
 def process_query(query):
-    # procesa las sentencias de la query para que no haya ; y se puedan ejecutar una a una
-    # devuelve una lista de sentencias
-    # separamos las sentencias
+    # process sentences so ; does not appear (python conflict)
+    # we do this to execute the sentences one by one
+    # return list of sentences
+
+    # split sentences
     lst_sql_sentences = query.split(';')
     for i in range(len(lst_sql_sentences)):
-        # quitamos ; para que pueda ejecutarse => errores si hay ; al final, interfiere con python
-        sentencia = lst_sql_sentences[i].replace(';', '')
-        sentencia = sentencia.split('*/')[-1] # quitamos los comentarios
-        sentencia = sentencia.replace('\n', ' ') # quitamos saltos de linea
-        sentencia = sentencia.rstrip().lstrip() # quitamos espacios al principio y final
+        sentencia = lst_sql_sentences[i].replace(';', '') # drop ; 
+        sentencia = sentencia.split('*/')[-1] # drop comments
+        sentencia = sentencia.replace('\n', ' ') # drop line jump
+        sentencia = sentencia.rstrip().lstrip() # drop blank spaces 
         lst_sql_sentences[i] = sentencia
         
-    for i in range(len(lst_sql_sentences)): # por si coge mal la sentencia
+    for i in range(len(lst_sql_sentences)): # double check
         if lst_sql_sentences[i].replace(' ', '') in ['\n', '']:
             lst_sql_sentences.pop(i)
             
@@ -60,43 +62,45 @@ def process_query(query):
             
 
 
-def sql_get_table(query): # ejecuta una sentencia sql que devuelve una tabla
-    oracledb.init_oracle_client() # inicia el cliente
+def sql_get_table(query):
+    # execute one sentence, return pd.DataFrame
+    oracledb.init_oracle_client() # start client
 
     user, passw, db = get_credentials()
-    # credenciales
+    # credentials
     un = user
     pw = passw
     cs = db
 
-    # creamos conexion
+    # create connection
     with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
-        start = dt.datetime.today() # inicio de ejecucion
-        df = pd.read_sql(sql=query, con=connection) # descargamos la db a pd.DataFrame()
-        print('\nEXEC TIME:', dt.datetime.today() - start) # mostramos tiempo de descarga
+        start = dt.datetime.today() # exec start
+        df = pd.read_sql(sql=query, con=connection) # download db to pd.DataFrame()
+        print('\nEXEC TIME:', dt.datetime.today() - start)
         print('\nTamaño del df: {:,.0f}'.format(len(df)).replace(',', '.'))
     return df
 
 
-def sql_exec_sentence(query): # ejecuta una sentencia sql cualquiera y no almacena el resultado
-    oracledb.init_oracle_client() # inicia el cliente
+def sql_exec_sentence(query):
+    # execute one sentence and do not save return
+    oracledb.init_oracle_client() # start client
     
     user, passw, db = get_credentials()
-    # credenciales
+    # credentials
     un = user
     pw = passw
     cs = db
-    # creamos conexion
+    # create conection
     with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
         with connection.cursor() as cursor:
             cursor.execute(query)
     
 
 def execute_complete_process(lst_sql_sentences):
-    # le pasamos una lista procesada de la funcion: process_query
+    # lst_sql_sentences is the return from process_query()
     df = pd.DataFrame()
     for sentence in lst_sql_sentences:
-        if sentence.split(' ')[0].upper() == 'SELECT': # devuelve tabla
+        if sentence.split(' ')[0].upper() == 'SELECT': # return TABLE
             df = sql_get_table(sentence)
         else: # CREATE, DROP, ....
             sql_exec_sentence(sentence)
@@ -107,36 +111,36 @@ def df_to_sql(df, name):
     print('\nCreando la tabla')
     start = dt.datetime.now()
     user, passw, db = get_credentials()
-    # credenciales
+    # credentials
     un = user
     pw = passw
     cs = db
     
-    # formato uri: "oracle://user:password@host"
+    # uri format: "oracle://user:password@host"
     uri = "oracle://" + un + ':' + pw + '@' + cs
     
     engine = sqla.create_engine(uri)
+
+    # by default pandas use TEXT type for object/string => SQL maps text into CLOB or TEXT
+    # we want SQL to map as CHAR
+    cols = df.dtypes[df.dtypes=='object'].index # df string columns
     
-    # por defecto pandas usa TEXT type para object/string => SQL lo mapea como CLOB o TEXT
-    # para que lo lea como CHAR hacemos lo siguiente:
-    cols = df.dtypes[df.dtypes=='object'].index # columnas de df que son string
-    
-    measurer = np.vectorize(len) # medidor de longitud de string vectorizado
-    # miramos la longitud maxima de las columnas que son string
+    measurer = np.vectorize(len) # length vectorize measurer
+    # max length in the string columns
     dic_str_max_len = {}
     for col in cols:
         res = measurer(df[col].values.astype(str)).max(axis=0)
         dic_str_max_len[col] = res
-    # indicamos que queremos que sea formato sql string no texto, con tamaño maximo calculado
+    # stablish the string format
     type_mapping = {col : sqla.types.String(dic_str_max_len[col]) for col in cols}
-    
-    # miramos si la tabla ya esta creada, en ese caso hacemos DROP antes de cargar la nueva
-    # lista de todas las tablas disponibles para el usuario
+
+    # search if table is created, in that case do DROP before CREATE
+    # list all table from user
     tablas = sql_get_table("SELECT table_name FROM user_tables")['TABLE_NAME'].values
     
-    if name in tablas: # si la tabla ya esta cargada da error y to_sql no lo detecta por permisos
+    if name in tablas:
         sql_exec_sentence("DROP TABLE " + name)
         print('\nTabla anterior eliminada')
-    # creamos la tabla
+    # CREATE TABLE
     df.to_sql(name, engine, if_exists='replace', dtype=type_mapping, index=False)
     print('\nEXEC time:', dt.datetime.now() - start)
